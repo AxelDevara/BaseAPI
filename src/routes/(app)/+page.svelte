d168e8ab7329702aef90b1d527ea3d0f9f1c660c
AxelDevara <axeldevara@gmail.com>
2023-08-07 20:16:20 +0700

    tailwind, ABI, and folder setup

---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,67 @@
+<div class="px-4">
+    <section
+        class="bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]"
+    >
+        <div
+            class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative"
+        >
+            <a
+                href="#"
+                class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-blue-700 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800"
+            >
+                <span
+                    class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3"
+                    >Beta</span
+                >
+                <span class="text-sm font-medium"
+                    >v0.1 Has just launched! See what's new</span
+                >
+                <svg
+                    class="w-2.5 h-2.5 ml-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 6 10"
+                >
+                    <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="m1 9 4-4-4-4"
+                    />
+                </svg>
+            </a>
+            <h1
+                class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+            >
+                Devs, it's 2023, where public API?
+            </h1>
+            <p
+                class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200"
+            >
+                We are web3 devs, famous for building 'public' platforms. Yet,
+                there are no public API for simple things like checking how much
+                you own on a scam ERC-20 contract. It's time for a change!
+            </p>
+        </div>
+        <div
+            class="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"
+        />
+    </section>
+
+    <a
+        href="#"
+        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-neutral-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    >
+        <h5
+            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+            Noteworthy technology acquisitions 2021
+        </h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+        </p>
+    </a>
+</div>
