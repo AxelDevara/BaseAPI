b4d141e02eb18489a2c5af12253d69bd43f42931
AxelDevara <axeldevara@gmail.com>
2023-08-09 19:12:53 +0700

    added database connection and more folder restructuring

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,5 @@
-// place files you want to import through the `$lib` alias in this folder.
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from '$env/static/public';
+
+// Initialize the JS client
+import { createClient } from '@supabase/supabase-js';
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY);
