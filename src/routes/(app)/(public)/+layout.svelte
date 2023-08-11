e6a64e1c1bf5ef9041315203d02f7578f93c9277
AxelDevara <axeldevara@gmail.com>
2023-08-11 10:36:55 +0700

    UI Changes

---
+++ src/routes/(app)/(public)/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { TableOfContents } from "@skeletonlabs/skeleton";
+    import { page } from "$app/stores";
+</script>
+
+<aside
+    class="fixed border-l pt-4 px-3 bg-gray-800 bg-opacity-50 border-neutral-700 h-full top-14 right-0 z-40 w-72"
+    aria-label="Sidebar"
+>
+    {#key $page.url.pathname}
+        <TableOfContents  label="Actions" target="#toc-target" />
+    {/key}
+</aside>
+
+<div class="mr-72 p-4">
+    <ol class="breadcrumb">
+        <li class="crumb"><a class="anchor" href={$page.url.pathname}>{$page.url.pathname.toUpperCase()}</a></li>
+    </ol>
+    <slot />
+</div>
+
---
+++ src/routes/(app)/(public)/erc20/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+
+    import { CodeBlock } from "@skeletonlabs/skeleton";
+    import { page } from "$app/stores";
+</script>
+<div id="toc-target">
+    <h2 class="mt-4 h3 mb-3">Overview</h2>
+    <hr />
+    <p class="mt-3">A set of API endpoints to call erc-20 based read functions.</p>
+    <CodeBlock language="Plaintext" code={$page.url.origin + "/public/v1/{action}"}></CodeBlock>
+</div>
